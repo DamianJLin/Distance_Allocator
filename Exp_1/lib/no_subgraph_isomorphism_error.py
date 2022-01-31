6883ea3753a90a68e2873062bf1a6fa5266627aa
Damian Lin <damianlin@iinet.net.au>
2022-01-31 21:37:55 +1100

    Add mapping parallel qubits, graphical output

---
+++ Exp_1/lib/no_subgraph_isomorphism_error.py
@@ -0,0 +1,2 @@
+class NoSubgraphIsomorphismError(ValueError):
+    pass
--- Exp_1/main.py
+++ Exp_1/main.py
@@ -2,11 +2,9 @@ from pathlib import Path
 import re
 import itertools
 import graph_tool.all as gt
-
-
-class NoSubgraphIsomorphismError(ValueError):
-    pass
-
+from lib.layer import layer
+from lib.no_subgraph_isomorphism_error import NoSubgraphIsomorphismError
+from lib.save_embedding import save_embedding_image
 
 ag_dir = Path(__file__).resolve().parent / 'arch_graphs'
 circ_dir = Path(__file__).resolve().parent / 'circuits'
@@ -34,49 +32,73 @@ for ag_path, circ_path in itertools.product(ag_paths, circ_paths):
     sub.set_directed(False)
     sub.set_fast_edge_removal(fast=True)
 
+    iso_map = None
+
     # Keeping track of logical qubits by their number in the qasm files. This is because graph_tool node ids are always
     # ordered starting from 0.
     qubit_by_qasm_id = {}
 
-    i = 0
-    while True:
-        # or maybe for u, v in circuit [i:] .. break
-        u, v = circuit[i]
-        # Add necessary edges and nodes to graph.
-        u_already_qubit = bool(u in qubit_by_qasm_id)
-        v_already_qubit = bool(v in qubit_by_qasm_id)
-        match (u_already_qubit, v_already_qubit):
-            # u and v
-            case (True, True):
-                e = sub.add_edge(qubit_by_qasm_id[u], qubit_by_qasm_id[v])
-            # u but not v (and vice versa)
-            case (True, False) | (False, True):
-                if v in qubit_by_qasm_id:  # Assume u is the one assigned
-                    u, v = v, u
-                qubit_by_qasm_id[v] = sub.add_vertex()
-                e = sub.add_edge(qubit_by_qasm_id[u], qubit_by_qasm_id[v])
-            # Neither u nor v
-            case (False, False):
-                qubit_by_qasm_id[u] = sub.add_vertex()
-                qubit_by_qasm_id[v] = sub.add_vertex()
-                e = sub.add_edge(qubit_by_qasm_id[u], qubit_by_qasm_id[v])
-            case _:
-                e = None
-        try:
-            iso_map = gt.subgraph_isomorphism(sub, ag, max_n=1, induced=False)
-            if not iso_map:
-                raise NoSubgraphIsomorphismError('No subgraph isomorphism between sub and ag.')
-        # If there was no isomorphism, we need to clean up edges and vertices we tried to add, then break.
-        except NoSubgraphIsomorphismError:
+    # Find maximal top sublist (largest circuit[:n] that can still be embedded as a subgraph).
+    for i, (u_layer, v_layer) in enumerate(circuit):
+
+        iso_found_in_layer = False
+
+        for j, (u, v) in enumerate(layer(circuit[i:])):
+            # Add necessary edges and nodes to graph.
+            u_already_qubit = bool(u in qubit_by_qasm_id)
+            v_already_qubit = bool(v in qubit_by_qasm_id)
             match (u_already_qubit, v_already_qubit):
+                # u and v
                 case (True, True):
-                    sub.remove_edge(e)
+                    e = sub.add_edge(qubit_by_qasm_id[u], qubit_by_qasm_id[v])
+                # u but not v (and vice versa)
                 case (True, False) | (False, True):
-                    sub.remove_edge(e)
-                    sub.remove_edge(v)
+                    if v in qubit_by_qasm_id:  # Assume u is the one assigned
+                        u, v = v, u
+                    qubit_by_qasm_id[v] = sub.add_vertex()
+                    e = sub.add_edge(qubit_by_qasm_id[u], qubit_by_qasm_id[v])
+                # Neither u nor v
                 case (False, False):
-                    sub.remove_edge(e)
-                    sub.remove_vertex(v)
-                    sub.remove_vertex(u)
+                    qubit_by_qasm_id[u] = sub.add_vertex()
+                    qubit_by_qasm_id[v] = sub.add_vertex()
+                    e = sub.add_edge(qubit_by_qasm_id[u], qubit_by_qasm_id[v])
+                case _:
+                    e = None
+            try:
+                iso_map = gt.subgraph_isomorphism(sub, ag, max_n=1, induced=False)
+                if iso_map:
+                    iso_found_in_layer = True
+                    break
+                else:
+                    raise NoSubgraphIsomorphismError('No subgraph isomorphism between sub and ag.')
+            # If there was no isomorphism, we need to clean up edges and vertices we tried to add, then break.
+            except NoSubgraphIsomorphismError:
+                match (u_already_qubit, v_already_qubit):
+                    case (True, True):
+                        sub.remove_edge(e)
+                    case (True, False) | (False, True):
+                        sub.remove_edge(e)
+                        sub.remove_vertex(v)
+                    case (False, False):
+                        sub.remove_edge(e)
+                        sub.remove_vertex(v)
+                        sub.remove_vertex(u)
+        if iso_found_in_layer:
+            # Continue search with new mapping.
+            continue
+        else:
+            # Finish search.
             break
-        i += 1
+
+    # Save image to file
+    if iso_map:
+        save_embedding_image(
+            subgraph=sub,
+            graph=ag,
+            mapping=iso_map[0],
+            location=out_dir / (str(ag_path.stem).rstrip('.graphml') + '_' +
+                                str(circ_path.stem).rstrip('.qasm.txt') + '.pdf'),
+        )
+
+    iso_map_all = gt.subgraph_isomorphism(sub, ag, max_n=0, induced=False)
+
